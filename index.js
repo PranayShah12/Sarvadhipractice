ea038dda643d0bd10c41a304bee46af6db8703c7
pranay <shahp382@gmail.com>
2023-05-28 23:28:18 +0530

    new

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
 function form_valid() {
-
-    // debugger
     let name = document.form1.fname.value;
     if (name == "" || name == null) {
         document.getElementById("fmess").innerHTML = "Enter name";
@@ -10,58 +8,56 @@ function form_valid() {
         document.getElementById("fmess").innerHTML = "Enter alphabets only";
         return false;
     }
-    let email = document.form1.mail.value
+
+    let email = document.form1.mail.value;
     if (email == "") {
         document.getElementById("emess").innerHTML = "Enter Email address";
         return false;
-    } else if (
-        !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
-    ) {
+    } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
         document.getElementById("emess").innerHTML = "You have entered an invalid email address!";
         return false;
     }
+
     let date = document.form1.myDate.value;
-    let lsdate = document.getElementById("myDate").value;
-    localStorage.setItem("lsdate", lsdate);
     let actualdate = new Date();
-    let realdate = `${actualdate.getFullYear()}-${actualdate.getMonth() + 1 < 10 ? `0${actualdate.getMonth() + 1}` : actualdate.getMonth() + 1}-${actualdate.getDate() < 10 ? `0${actualdate.getDate() + 1}` : actualdate.getDate()}`
+    let realdate = `${actualdate.getFullYear()}-${actualdate.getMonth() + 1 < 10 ? `0${actualdate.getMonth() + 1}` : actualdate.getMonth() + 1}-${actualdate.getDate() < 10 ? `0${actualdate.getDate()}` : actualdate.getDate()}`;
     if (date == "") {
-        document.getElementById("dmess").innerHTML = "select date";
+        document.getElementById("dmess").innerHTML = "Select a date";
         return false;
-    } else if (date <= realdate) {
-        document.getElementById("errmess").innerHTML = "Invalid Date"
-        return false;
-    }
-
-    let ftime = document.form1.fromtime.value;
-
-    if (ftime == "") {
-        document.getElementById("ftmess").innerHTML = "select from time";
-        return false;
-    }
-    let utime = document.form1.totime.value;
-    if (utime == "") {
-        document.getElementById("totmess").innerHTML = "select to time";
-        return false;
-    }
-    // if (utime <= ftime) {
-    //     document.getElementById("timemess").innerHTML = "To time Should be Greater then from time"
-    //     return false;
-    // }
-    let desp = document.form1.despval.value;
-    if (desp == "") {
-        document.getElementById("despmess").innerHTML = "enter Description";
+    } else if (date < realdate) {
+        document.getElementById("errmess").innerHTML = "Invalid Date";
         return false;
     }
 
-    alert("Appointmnet is submitted")
-    location.href = "/Users/sarvadhi/Desktop/AppointmentDemo/appointment.html"
+    let fromTime = document.form1.fromtime.value;
+    if (fromTime == "") {
+        document.getElementById("ftmess").innerHTML = "Select from time";
+        return false;
+    }
+
+    let toTime = document.form1.totime.value;
+    if (toTime == "") {
+        document.getElementById("totmess").innerHTML = "Select to time";
+        return false;
+    } else if (toTime <= fromTime) {
+        document.getElementById("totmess").innerHTML = "To time should be greater than From time";
+        return false;
+    }
+
+    let description = document.form1.despval.value;
+    if (description == "") {
+        document.getElementById("despmess").innerHTML = "Enter Description";
+        return false;
+    }
+
+    // Form is valid, allow submission
+    return true;
 }
 
 
 function on_change() {
     let name = document.form1.fname.value;
-    if (name != "" || name != null) {
+    if (name != "" && name != null) {
         document.getElementById("fmess").innerHTML = "";
         return false;
     } else if (isNaN(name)) {
@@ -69,55 +65,76 @@ function on_change() {
         return false;
     }
 }
+function on_change() {
+    let name = document.form1.fname.value;
+    if (name != "" || name != null) {
+        document.getElementById("fmess").innerHTML = "";
+    } else if (isNaN(name)) {
+        document.getElementById("fmess").innerHTML = "";
+    }
+    return true; // Return true when the input is valid
+}
+
 function mailchange() {
-    let email = document.form1.mail.value
+    let email = document.form1.mail.value;
     if (email != "") {
         document.getElementById("emess").innerHTML = "";
-        return false;
-    } else if (
-        !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
-    ) {
+    } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
         document.getElementById("emess").innerHTML = "";
-        return false;
     }
+    return true; // Return true when the input is valid
 }
+
 function datechange() {
-    let date = document.form1.myDate.value;
-    if (date != "") {
+    let date = new Date(document.form1.myDate.value);
+    let currentDate = new Date(); // Get current date
+
+    if (date.getTime() >= currentDate.getTime()) {
         document.getElementById("dmess").innerHTML = "";
-        return false;
-    }
-    if ((date <= realdate) == false) {
         document.getElementById("errmess").innerHTML = "";
-        return false;
+    } else {
+        document.getElementById("dmess").innerHTML = "Invalid date";
+        document.getElementById("errmess").innerHTML = "";
+        return false; // Return false when the date is invalid
     }
+    return true; // Return true when the input is valid
 }
+
 function f_timechange() {
     let ftime = document.form1.fromtime.value;
     if (ftime != "") {
         document.getElementById("ftmess").innerHTML = "";
-        return false;
     }
+    return true; // Return true when the input is valid
 }
+
+
 function to_timechange() {
-    let utime = document.form1.totime.value;
-    if (utime != "") {
-        document.getElementById("totmess").innerHTML = "";
-        return false;
+    let fromTime = document.form1.fromtime.value;
+    let toTime = document.form1.totime.value;
+
+    if (toTime !== "" && fromTime !== "") {
+        if (toTime <= fromTime) {
+            document.getElementById("totmess").innerHTML = "To time should be greater than From time";
+            return false;
+        } else {
+            document.getElementById("totmess").innerHTML = "";
+        }
     }
+
+    return true;
 }
+
+
 function desp_change() {
     let desp = document.form1.despval.value;
     if (desp != "") {
         document.getElementById("despmess").innerHTML = "";
-        return false;
     }
+    return true;
 }
 // const newform = document.getElementById('app_book');
 // console.log(newform)
-function takevalue() {
-    var fname = document.getElementById("fname").value;
-    document.getElementById("info").innerHTML = fname;
-}
+
 
 
--- response.js
+++ response.js
@@ -1,13 +1,15 @@
 
-function updateTableData() {
+window.addEventListener("DOMContentLoaded", () => {
     const task = JSON.parse(localStorage.getItem("task"))
 
-    const tabledata = document.getElementById("tabledata");
-    tabledata.innerHTML = "";
 
-    for (let i = 0; i < task.length; i++) {
-        const row = document.createElement("tr");
-        row.innerHTML = `
+    if (task && task.length > 0) {
+
+        const tabledata = document.getElementById("tabledata");
+
+        for (let i = 0; i < task.length; i++) {
+            const row = document.createElement("tr");
+            row.innerHTML = `
             <td>${i + 1}</td>
             <td>${task[i].name}</td>
             <td>${task[i].mail_id}</td>
@@ -16,7 +18,8 @@ function updateTableData() {
             <td>${task[i].time}</td>
             <td>${task[i].Despvalue}</td>`
 
-        tabledata.appendChild(row);
-    }
+            tabledata.appendChild(row);
+        }
 
-}
+    }
+})
